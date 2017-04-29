849db996e7acaaec201a3a799e62dcd81d8742d7
Kirill Demidov <kademidov@gmail.com>
2017-04-29 20:10:32 +0300

    блок review готов

--- src/js/scripts/script.js
+++ src/js/scripts/script.js
@@ -49,8 +49,30 @@ $(document).ready(function(){
     });
   }
 
+  function ratioLetter(block) {
+    $(block).each(function(){
+      var itemWidth = $(this).width();
+      var itemHeight = $(this).height();
+      var itemRatio = itemHeight / itemWidth;
+
+      var photo = $(this).find('img');
+      var photoWidth = photo.width();
+      var photoHeight = photo.height();
+      var photoRatio = photoHeight / photoWidth;
+
+      if ( itemRatio > photoRatio ) {
+        photo.width('100%');
+      } else if (itemRatio < photoRatio) {
+        photo.height('100%');
+      } else {
+        photo.width('100%');
+      }
+    });
+  }
+
   ratioPhoto('.review__slider-preview-item');
   ratioPhoto('.review__slider-big-item');
-  
+  ratioLetter('.review__letter-box');
+  ratioLetter('.review__chart-box');
 
 });
