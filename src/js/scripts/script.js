d4cd666333b0386901c3c86d7f92c756011d2643
Kirill Demidov <kademidov@gmail.com>
2017-05-22 15:45:17 +0300

    Типы проекта и помплектация

--- src/js/scripts/script.js
+++ src/js/scripts/script.js
@@ -84,15 +84,16 @@ $(window).load(function () {
   ratioPhoto('.review-card__visual');
   ratioPhoto('.project__photo');
 
-  function tabs(boxTabsLink, boxTabsContent){ 
+  function tabs(boxTabsLink, boxTabsContent, tabs){ 
     $(boxTabsLink + ' input').on('click', function(e){
       var attrLink = $(this).attr('id');
-      $(boxTabsContent + ' .project-form__table-select').css("display", "none");
+      console.log(attrLink);
+      $(boxTabsContent + ' ' + tabs).css("display", "none");
       $(boxTabsContent + ' .' + attrLink).css("display", "block");
- 
     }); 
   };
 
 
-  tabs('#tabs-roof', '#tabs-roof-content');
+  tabs('#tabs-roof', '#tabs-roof-content', '.project-form__table-select--roof-content');
+  tabs('#tabs-frame', '#tabs-frame-content', '.project-form__tab-item');
 });
