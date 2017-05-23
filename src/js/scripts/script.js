c334d1fd6c55fab704c62c2ff2ef86666a0221f2
Kirill Demidov <kademidov@gmail.com>
2017-05-23 13:30:14 +0300

    header adaptive and scroll animation

--- src/js/scripts/script.js
+++ src/js/scripts/script.js
@@ -96,4 +96,74 @@ $(window).load(function () {
 
   tabs('#tabs-roof', '#tabs-roof-content', '.project-form__table-select--roof-content');
   tabs('#tabs-frame', '#tabs-frame-content', '.project-form__tab-item');
+
+
+
+  // бургер, что тут еще скажешь? но без колы(
+  $('.burger').on('click', function(e){
+      e.preventDefault();
+      if ( $(this).hasClass("burger--close") ) {
+          $(this).removeClass('burger--close');
+          $('.main-nav').slideUp(400);
+          $('.main-nav').removeClass('main-nav--open');
+          $('.page-header').removeClass('page-header--open');
+      } else {
+          $(this).addClass('burger--close');
+          $('.main-nav').slideDown(400);
+          $('.main-nav').addClass('main-nav--open');
+          $('.page-header').addClass('page-header--open');
+      }
+  });
+
+  $('.nav ul a').on('click', function(e){
+      if ($(window).width() < 1200) {
+          $('.main-nav').slideUp(400);
+          $('.burger').addClass('burger--open');
+          $('.burger').removeClass('burger--close');
+      };
+  });
+
+
+  $(window).resize(function(){
+      if( $(window).width() > 1199 ) {
+          $('.main-nav').show();
+      } else {
+          $('.main-nav').slideUp(400);
+          $('.main-nav').removeClass('main-nav--open');
+          $('.page-header').removeClass('page-header--open');
+          $('.burger').removeClass('burger--close');
+      }
+  });
+
+  $(window).on('scroll', function(e) { // отслеживаем событие на элементе window
+    if( $(window).width() < 1200 ) {
+        $('.main-nav').slideUp(400);
+        $('.main-nav').removeClass('main-nav--open');
+        $('.page-header').removeClass('page-header--open');
+        $('.burger').removeClass('burger--close');
+    }
+  });
+
+  if ( $(window).scrollTop() > 100 && $(window).width() > 1199) {
+    $('.page-header').addClass('page-header--scroll');
+    $('.page-header').addClass('page-header--desktop');
+  } else if ( $(window).scrollTop() > 100 ) {
+    $('.page-header').addClass('page-header--scroll');
+  }
+
+  $(window).on('scroll', function(e) {
+
+    if ( $(window).scrollTop() > 100 && $(window).width() > 1199 ) {
+      $('.page-header').addClass('page-header--scroll');
+      $('.page-header').addClass('page-header--desktop');
+    } else if ( $(window).scrollTop() > 100 ) {
+      $('.page-header').addClass('page-header--scroll');
+      $('.page-header').removeClass('page-header--desktop');
+    } else {
+      $('.page-header').removeClass('page-header--scroll');
+      $('.page-header').removeClass('page-header--desktop');
+    }
+
+  });
+
 });
