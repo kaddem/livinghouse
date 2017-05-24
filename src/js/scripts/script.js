1a61a4b71ad5e539a5c93e47a7df05f80cb99276
Kirill Demidov <kademidov@gmail.com>
2017-05-24 21:42:37 +0300

    input mask для телефона. Увеличение расстояний активных элементов для телефонов. Изменение расположения элементов для мобилки, после теста

--- src/js/scripts/script.js
+++ src/js/scripts/script.js
@@ -196,4 +196,11 @@ $(window).load(function () {
 
   });
 
+  function telMask(selector) {
+    $(selector).mask('+7 (000) 000-00-00');
+  }
+
+  telMask('.contact-form__input--tel');
+  telMask('.consult__input-tel');
+
 });
